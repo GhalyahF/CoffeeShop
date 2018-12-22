ba3585866d406dbc10fbc86d6b64064fc382576c
GhalyahF <gaaa291@nyu.edu>
2018-12-22 22:06:39 +0300

    authentication

--- mycoffee/views.py
+++ mycoffee/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserSignUp, UserLogin
+from django.contrib.auth import authenticate,login,logout
 
-# Create your views here.
+
+
+def usersignup(request):
+    form= UserSignUp()
+    if request.method == 'POST':
+        form= UserSignUp(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username= user.username
+            password= user.password
+
+            user.set_password(password)
+            user.save()
+
+            auth_user= authenticate(username=username, password=password)
+            login(request, auth_user)
+            return redirect('/')
+        return redirect("mycoffee:signup")
+    context={
+    'form': form,
+    }
+    return render(request, 'sigup.html', context)
+
+def userlogin(request):
+        form= UserLogin()
+        if request.method == 'POST':
+            form= UserLogin(request.POST)
+            if form.is_valid():
+
+                username= form.cleaned_data['username']
+                password= form.cleaned_data['password']
+
+                auth_user= authenticate(username=username, password=password)
+                if auth_user is not None:
+                    login(request, auth_user)
+                    return redirect('/')
+                return redirect("mycoffee:login")
+        context={
+        'form': form,
+        }
+        return render(request, 'login.html', context)
+
+
+def userlogout(request):
+    logout(request)
+    return redirect("/")
