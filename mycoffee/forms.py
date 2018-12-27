fa9c5e6b3e7ddd88fef5537bbcb9cab3c23e46d4
GhalyahF <gaaa291@nyu.edu>
2018-12-27 15:02:13 +0300

    customizing a coffee

--- mycoffee/forms.py
+++ mycoffee/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Coffee
 
 class UserSignUp(forms.ModelForm):
     class Meta:
@@ -14,4 +15,9 @@ class UserSignUp(forms.ModelForm):
 class UserLogin(forms.Form):
     username= forms.CharField(required= True)
     password= forms.CharField(required=True, widget= forms.PasswordInput())
-    
+
+class CoffeeForm(forms.ModelForm):
+    class Meta:
+        model=Coffee
+        fields = "__all__"
+        exclude = ['user', 'price']
--- mycoffee/urls.py
+++ mycoffee/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('signup/', views.usersignup, name="signup"),
     path('login/', views.userlogin, name="login"),
     path('logout/', views.userlogout, name="logout"),
+
+    path('create_coffee/', views.create_coffee, name='create_coffee'),
 ]
